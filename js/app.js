be5eaea3dc307d3bf15e42a23efef4b39eeca5f6
ABiviano13 <abiviano0@outlook.com>
2023-02-13 15:14:33 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Creare un alert dove ci sono scritti 5 numeri random
+//1. Creare 5 numeri random
+//2. Inserire i 5 numeri in un alert
+
+//Allo scadere dell'alert creare un timer di 30 secondi
+
+//Allo scadere del timer creare un prompt dove l'utente dovrà scrivere i 5 numeri che ha letto nell'alert
+
+//Individuare quanti numeri sono stati indovinati e quanti no
