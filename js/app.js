0602103eabab6c8aeeba7cc2d260183da208212c
ABiviano13 <abiviano0@outlook.com>
2023-02-13 15:24:47 +0100

    FEAT creazione numeri random + alert

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 //Creare un alert dove ci sono scritti 5 numeri random
 //1. Creare 5 numeri random
+const arrayNumeriRandom = [];
+// console.log(arrayNumeriRandom);
+
+for(let i = 0; i < 5; i++){
+    const numeroRandom = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+    // console.log(numeroRandom);
+
+    arrayNumeriRandom.push(numeroRandom);
+}
 //2. Inserire i 5 numeri in un alert
+alert(arrayNumeriRandom);
 
 //Allo scadere dell'alert creare un timer di 30 secondi
 
